663be08c663671d1b5ea55ae4b01f53d31a78b1f
larizza <larizza@mctekk.com>
2021-04-01 06:12:56 -0400

    feat(prompts): add initial questions

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,40 +1,72 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const Octokit = require("@octokit/core").Octokit;
+
+const octokit = new Octokit();
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the brilliant ${chalk.red('generator-obsidian-plugin')} generator!`
-      )
-    );
-
-    const prompts = [
+  async prompting() {
+    const obsidianVersions = await this._getObsidianVersions();
+    this.answers = await this.prompt([
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: "input",
+        name: "id",
+        message: "Id"
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Name"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Description"
+      },
+      {
+        type: "input",
+        name: "pluginVersion",
+        message: "version",
+        default: "1.0.0"
+      },
+      {
+        type: "list",
+        name: "minObsidianVersion",
+        message: "Minimum Obsidian version required",
+        choices: obsidianVersions
+      },
+      {
+        type: "input",
+        name: "authorName",
+        message: "Author's Name"
+      },
+      {
+        type: "input",
+        name: "authorHomepage",
+        message: "Author's Homepage"
+      },
+      {
+        type: "list",
+        name: "pluginIsDesktopOnly",
+        message: "Is desktop only?",
+        choices: ["true", "false"]
       }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    ]);
   }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+  async _getObsidianVersions() {
+    const { data: releases = [] } = await octokit.request(
+      "GET /repos/{owner}/{repo}/releases",
+      {
+        owner: "obsidianmd",
+        repo: "obsidian-releases"
+      }
     );
-  }
-
-  install() {
-    this.installDependencies();
+    const versions = releases
+      .filter(
+        release => release.prerelease === false && release.draft === false
+      )
+      .map(release => release.tag_name);
+    return versions;
   }
 };
